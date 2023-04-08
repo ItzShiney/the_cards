3d2554a120da1402df735dcadc4c9ba88d08196c
Shiney <stopthewalkingdead@gmail.com>
2023-04-08 18:36:11 +0300

    add `АЙОЙ`

---
+++ src/chrs/айой.rs
@@ -0,0 +1,29 @@
+pub use crate::card_uses::*;
+
+pub fn name() -> CustomString {
+    cs!["АЙОЙ"]
+}
+
+#[rustfmt::skip]
+pub fn groups() -> Groups {
+    GroupsBuilder {
+        tier: D,
+        author: ByЛёня,
+        genders: [Женщина],
+        tags: [Nichijou],
+    }.into()
+}
+
+// 3/1/-inf
+#[rustfmt::skip]
+pub fn stats() -> Stats {
+    Stats::new(
+        phy!(4),
+        dmg!(1),
+        int!(0 = const),
+    )
+}
+
+pub fn description() -> CustomString {
+    cs![Epitaph(cs!["\"супер-ультра-громадно-восхитительно-невероятно-непонятно\""])]
+}
