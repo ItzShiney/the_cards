0104692e8ffd5a8235c60304e41401bd0a79c843
Shiney <stopthewalkingdead@gmail.com>
2023-04-09 17:43:56 +0300

    add `ISAAC`

---
+++ src/chrs/isaac.rs
@@ -0,0 +1,25 @@
+pub use crate::card_uses::*;
+
+pub fn name() -> CustomString {
+    cs!["ISAAC"]
+}
+
+#[rustfmt::skip]
+pub fn groups() -> Groups {
+    GroupsBuilder {
+        tier: D,
+        author: ByЛёня,
+        genders: [Мужчина],
+        tags: [TBoI],
+    }.into()
+}
+
+// 2/1/-2
+#[rustfmt::skip]
+pub fn stats() -> Stats {
+    Stats::new(
+        phy!(4),
+        dmg!(3),
+        int!(6),
+    )
+}
