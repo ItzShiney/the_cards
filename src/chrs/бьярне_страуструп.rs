d8fa57869306892b29d9a34c953a04434c33a6a9
Shiney <stopthewalkingdead@gmail.com>
2023-06-02 15:34:36 +0300

    add `БЬЯРНЕ СТРАУСТРУП`

---
+++ src/chrs/бьярне_страуструп.rs
@@ -0,0 +1,55 @@
+pub use crate::card_uses::*;
+
+pub fn name() -> CustomString {
+    cs!["БЬЯРНЕ СТРАУСТРУП"]
+}
+
+#[rustfmt::skip]
+pub fn groups() -> Groups {
+    GroupsBuilder {
+        tier: S,
+        author: ByМаксим,
+        genders: [Мужчина],
+        tags: [Программирование],
+    }.into()
+}
+
+#[rustfmt::skip]
+pub fn stats() -> Stats {
+    Stats::new(
+        phy!(6),
+        dmg!(9),
+        int!(9 = const),
+    )
+}
+
+pub fn description() -> CustomString {
+    cs![
+        NamedPoint(cs!["ВЕЧНЫЙ ПРОГРАММИСТ"], cs![Intellect, " ", Const(0)]),
+        __,
+        Condition(cs!["закончилась битва"]),
+        NamedPoint(
+            cs!["ZERO COST ABSTRACTIONS"],
+            cs!["использованные на него владельцем активки возвращаются в руку"],
+        ),
+        __,
+        Condition(cs!["противник умер"]),
+        NamedPoint(cs!["BEST PRACTICES"], cs!["его тир..."]),
+        Tab,
+        Point(cs!["C, D ", Implies, " уничтожается"]),
+        Tab,
+        Point(cs!["A, B ", Implies, " переходит владельцу этой карты"]),
+        __,
+        Condition(cs!["противник атакует"]),
+        NamedPoint(cs!["C++"], cs![Intellect, " противника..."]),
+        Tab,
+        Point(cs!["0..3 ", Implies, " атакует сам себя"]),
+        Tab,
+        Point(cs!["4..6 ", Implies, " не атакует"]),
+    ]
+}
+
+// TODO
+pub fn abilities() -> GameCallbacks {
+    GameCallbacks::default()
+}
